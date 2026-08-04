8317d1d366407f2807a3306c5fcaec4782045105
Ana <ana.hernandez.conejo.work@gmail.com>
2026-08-04 04:53:30 +0200

    Delete sources.py

--- sources.py
+++
@@ -1,7 +0,0 @@
-import feedparser
-RSS="https://bocyl.jcyl.es/boletines/rss.xml"
-def latest():
-    f=feedparser.parse(RSS)
-    if not f.entries: return None
-    e=f.entries[0]
-    return e.title,e.link
