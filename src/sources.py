19e0ea7fe64b99c9343705a0f147fbdca822322e
Ana <ana.hernandez.conejo.work@gmail.com>
2026-08-08 20:47:11 +0200

    Añadir AENA y ADIF al agregador

--- src/sources.py
+++ src/sources.py
@@ -1,7 +1,11 @@
 from ayuntamiento_valladolid import AyuntamientoValladolidSource
 from age import AgeSource
+from aena import AenaSource
+from adif import AdifSource
 
 SOURCES = [
     AyuntamientoValladolidSource(),
     AgeSource(),
+    AenaSource(),
+    AdifSource(),
 ]
